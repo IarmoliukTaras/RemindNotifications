a08ce37a1a1e65dc8eda6de1673253aa0dac2e33
Iarmoliuk Taras <iarmoliuktaras@gmail.com>
2017-10-24 11:23:18 +0300

    Initial Commit

---
+++ Remind(Notifications)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Remind(Notifications)
+//
+//  Created by 123 on 24.10.17.
+//  Copyright © 2017 123. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
