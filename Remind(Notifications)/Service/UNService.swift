8924c9f2498e1ad3fe96401b5d7d9e371537b5ae
Iarmoliuk Taras <iarmoliuktaras@gmail.com>
2017-10-24 12:29:01 +0300

    Setting up UNService

---
+++ Remind(Notifications)/Service/UNService.swift
@@ -0,0 +1,64 @@
+//
+//  UNService.swift
+//  Remind(Notifications)
+//
+//  Created by 123 on 24.10.17.
+//  Copyright © 2017 123. All rights reserved.
+//
+
+import Foundation
+import UserNotifications
+
+class UNService: NSObject {
+    
+    private override init() {}
+    static let shared = UNService()
+    
+    let unCenter = UNUserNotificationCenter.current()
+    
+    func authorize() {
+        let options: UNAuthorizationOptions = [.alert, .badge, .sound, .carPlay]
+        unCenter.requestAuthorization(options: options) { (granted, error) in
+            print(error ?? "No error")
+            guard granted else {
+                print("User Denied access")
+                return
+            }
+            self.configure()
+        }
+    }
+    
+    func configure() {
+        unCenter.delegate = self
+    }
+}
+
+
+
+extension UNService: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        print("UN did receive response")
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        print("UN will present")
+        
+        let options: UNNotificationPresentationOptions = [.alert, .sound]
+        completionHandler(options)
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
